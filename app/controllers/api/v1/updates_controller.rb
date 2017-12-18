168f793d75eac8923f199ed0d8111bee973ed68c
bing5tui3 <rui.oaoa@gmail.com>
2017-12-18 22:21:03 +0800

    update

--- app/controllers/api/v1/updates_controller.rb
+++ app/controllers/api/v1/updates_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::UpdatesController < ApplicationController
 	skip_before_action :verify_authenticity_token
+
 	def create
 		puts params
-		chat_id  = params['message']['chat']['id']
+		chat_id  = chat['id']
 		puts chat_id
-		text = params['message']['text']
+		text = message['text']
 		puts text
 
 		case text
@@ -27,4 +28,27 @@ class Api::V1::UpdatesController < ApplicationController
 
 		render json: {status: 'SUCCESS', message: 'Update Accepted'}, status: :ok
 	end
+
+	def message
+		params['message']
+	end
+
+	def chat
+		message['chat']
+	end
+
+	def from
+		message['from'] 
+	end
+
+	def user
+		@user ||=  User.find_by(telegram_id: from['id']) || register_user
+	end
+
+	def register_user
+		@user = User.find_or_initialize_by(telegram_id: from['id'])
+		@user.update_attributes!(first_name: from['first_name'], last_name: from['last_name'])
+		@user
+	end
+
 end
